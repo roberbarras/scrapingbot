3c6cd6bf44cbc44eaa154b1dd22e19390093efe8
roberbarras <roberbarras@gmail.com>
2021-04-07 23:41:42 +0200

    Initial commit

---
+++ src/main/java/org/telegram/bot/scrapingbot/ScrapingbotApplication.java
@@ -0,0 +1,13 @@
+package org.telegram.bot.scrapingbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapingbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapingbotApplication.class, args);
+	}
+
+}
