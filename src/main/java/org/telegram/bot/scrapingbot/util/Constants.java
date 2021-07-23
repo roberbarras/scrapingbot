8a984ed642ae7ae07a5604939b9cec9a9068095e
roberbarras <roberbarras@gmail.com>
2021-07-23 14:33:28 +0200

    Fix css class in Constants.java

--- src/main/java/org/telegram/bot/scrapingbot/util/Constants.java
+++ src/main/java/org/telegram/bot/scrapingbot/util/Constants.java
@@ -3,7 +3,7 @@ package org.telegram.bot.scrapingbot.util;
 public final class Constants {
 
     public static final String DISABLED_CLASS = "product-size-selector__size-list-item--is-disabled";
-    public static final String PRODUCT_LIST_CLASS = "product-size-selector__size-list";
+    public static final String PRODUCT_LIST_CLASS = "product-detail-size-selector__size-list";
     public static final String LIST_ELEMENT_CLASS = "li";
     public static final String KAFKA_OFFSET_CONFIG = "latest";
     public static final String KAFKA_PROTOCOL = "SASL_SSL";
