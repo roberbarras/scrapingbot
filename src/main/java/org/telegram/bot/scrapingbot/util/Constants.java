7e1187efe20e61a9a38626cc29fee48b67866fef
roberbarras <roberbarras@gmail.com>
2022-12-14 12:13:38 +0100

    Web tags changes

--- src/main/java/org/telegram/bot/scrapingbot/util/Constants.java
+++ src/main/java/org/telegram/bot/scrapingbot/util/Constants.java
@@ -2,8 +2,8 @@ package org.telegram.bot.scrapingbot.util;
 
 public final class Constants {
 
-    public static final String DISABLED_CLASS = "product-detail-size-selector__size-list-item--is-disabled";
-    public static final String PRODUCT_LIST_CLASS = "product-detail-size-selector__size-list";
+    public static final String DISABLED_CLASS = "size-selector__size-list-item--is-disabled";
+    public static final String PRODUCT_LIST_CLASS = "size-selector__size-list";
     public static final String LIST_ELEMENT_CLASS = "li";
     public static final String KAFKA_OFFSET_CONFIG = "latest";
     public static final String KAFKA_PROTOCOL = "SASL_SSL";
